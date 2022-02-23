af2bed2e45226997be88351c07677f068008b651
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-02-23 01:03:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,107 +1,66 @@
-require([
-      "esri/Map",
-      "esri/layers/FeatureLayer",
-      "esri/views/MapView",
+    require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+	   "esri/widgets/Home",
+      "esri/widgets/Legend",
+      "esri/widgets/LayerList",
       "dojo/domReady!"
-    ], function(
-      Map,
-      FeatureLayer,
-      MapView
-    ) {
-
-      // Create the map
-      var map = new Map({
-        basemap: "gray"
+      
+    ], function(WebScene, SceneView, Camera,Legend,LayerList, Home) {
+    
+ 
+      /*var map = new Map({
+        basemap: "streets",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+         id:"8046207c1c214b5587230f5e5f8efc77" 
+        }
       });
-
-      // Create the MapView
-      var view = new MapView({
+      
+      var camera = new Camera({
+        position: [
+           -71.060217,// lon
+          42.382655, // lat
+          2500// elevation in meters
+        ],
+        tilt:45,
+        heading: 180
+      })
+      var view = new SceneView({
         container: "viewDiv",
-        map: map,
-        center:[-90.1, 38.6],
-        zoom: 9
+        map: scene,
+        camera: camera
+    });
+	
+	var homeBtn = new Home({
+        view: view
       });
+      // Add the home button to the top left corner of the view
+    view.ui.add(homeBtn, "top-left");
+   
 
-      /*************************************************************
-       * The PopupTemplate content is the text that appears inside the
-       * popup. {fieldName} can be used to reference the value of an
-       * attribute of the selected feature. HTML elements can be used
-       * to provide structure and styles within the content. The
-       * fieldInfos property is an array of objects (each object representing
-       * a field) that is use to format number fields and customize field
-       * aliases in the popup and legend.
-       **************************************************************/
-
-      var template = { // autocasts as new PopupTemplate()
-        title: "Neighborhood: {NHD_NAME}",
-        content: [{
-          // It is also possible to set the fieldInfos outside of the content
-          // directly in the popupTemplate. If no fieldInfos is specifically set
-          // in the content, it defaults to whatever may be set within the popupTemplate.
-          type: "fields",
-          fieldInfos: [{
-            fieldName: "Shape__Area",
-            label: "Shape__Area: ",
-            visible: true
-          }, {
-            fieldName: "FID",
-            label: "FID: ",
-            visible: true,
-            format: {
-              digitSeparator: true,
-              places: 0
-            }
-          }, {
-            fieldName: "NHD_NUM ",
-            label: "NHD_NUM: ",
-            visible: true,
-            format: {
-              digitSeparator: true,
-              places: 0
-            }
-          }, {
-            fieldName: "NHD_NAME",
-            label: "NHD_NAME",
-            visible: true,
-            format: {
-              digitSeparator: true,
-              places: 0
-            }
-          }, {
-            fieldName: "ANGLE",
-            label: "ANGLE",
-            visible: true,
-            format: {
-              digitSeparator: true,
-              places: 0
-            }
-          }
-                      ]
-        }]
-      };
-
-     var symbol = {
-      type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
-      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Home-icon.svg/1200px-Home-icon.svg.png",
-      width: "16px",
-      height: "16px"
-};
-  var renderer = {
-      type: "simple",  // autocasts as new SimpleRenderer()
-      symbol: symbol
-    };
-  
-  
-      // Reference the popupTemplate instance in the
-      // popupTemplate property of FeatureLayer
-      var featureLayer = new FeatureLayer({
-        url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/ArcGIS/rest/services/STL_Neighborhood/FeatureServer/0",
-        outFields: ["*"],
-        popupTemplate: template,
-        renderer:renderer
-      });
-  
-      map.add(featureLayer);
-  
 
     });
+view.when(function() {
+	
+          // get the first layer in the collection of operational layers in the WebMap
+          // when the resources in the MapView have loaded.
+        var featureLayer = scene.layers.getItemAt(1);
+
+        var legend = new Legend({
+          view: view,
+          layerInfos: [{
+            layer: featureLayer,
+            title: "Major project buildings"
+          }]
+        });
+      
+   view.ui.add(legend, "bottom-right");
+   view.ui.add(layerList, "bottom-right");
+   });
+var layerList = new LayerList({
+  view: view
+});
